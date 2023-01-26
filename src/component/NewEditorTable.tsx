a3772f4251c735ddbf7a9d0926548f11ecf61cc0
vood <balitskY_igor@list.ru>
2023-01-26 13:30:03 +0300

    init

--- src/component/NewEditorTable.tsx
+++ src/component/NewEditorTable.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import {DataGrid} from "@mui/x-data-grid";
+import {DataGrid, GridToolbar} from "@mui/x-data-grid";
 import {useTableDataQuery} from "../store/cfg/admin.api";
-import {IDeleteTableData, ITableData} from "../models/table.model";
+import {ITableData} from "../models/table.model";
 import {GridEnrichedColDef} from "@mui/x-data-grid/models/colDef/gridColDef";
+import {GridToolbarColumnsButton} from "@mui/x-data-grid/components/toolbar/GridToolbarColumnsButton";
 
 
 export function NewEditorTable() {
@@ -22,7 +23,7 @@ export function NewEditorTable() {
                     return c;
                 }
             )
-        // getColumnsData(td)
+            .sort(q => q.editable===true? 1: 0)
         return cols as GridEnrichedColDef<any>[]
     }
 
@@ -33,60 +34,20 @@ export function NewEditorTable() {
     }
 
     const getColumnsData = (td: ITableData) => {
-        var voteCardsArray = [
-            { placeInfo: { id: 42, desc: 'stuff 42' } },
-            { placeInfo: { id: 65, desc: 'stuff 65' } },
-            { placeInfo: { id: 89, desc: 'stuff 89' } },
-        ];
-
-        var res = Object.assign({}, ...voteCardsArray.map(card => ({[card.placeInfo.id]: card.placeInfo.desc})));
-
-        console.log("res ",res)
-
-
-
         const primaryKeyColumns: string[] = primaryKeyList(td);
-        let undefineds = td.tableData
+        return td.tableData
             .map(entryMap => {
-                const idValue = primaryKeyColumns
-                    .map(pk =>{
-                            return entryMap.cols
-                                .filter(c=> c.colName === pk)
-                                .map(cv => cv.data)[0]
-                        }
-                    )
-                    .join("_")
-                const strings = Object.assign({id: idValue }, ...entryMap.cols.map(row => ({[row.colName]: row.data})));
-                return strings
+                    const idValue = primaryKeyColumns
+                        .map(pk => {
+                                return entryMap.cols
+                                    .filter(c => c.colName === pk)
+                                    .map(cv => cv.data)[0]
+                            }
+                        )
+                        .join("_")
+                    return Object.assign({id: idValue}, ...entryMap.cols.map(row => ({[row.colName]: row.data})))
                 }
-            );
-        return undefineds
-    }
-
-
-    const asd =  {
-            id: 'asd',
-
-            beg_node_id: 'uasp-streaming-unp-convertor~way4-issuing-operation',
-            beg_node_type: 'flink_srv',
-            common_name: 'Не задан',
-            end_node_id: 'dev_ivr__uasp_realtime__input_converter__way4_issuing_operation__json',
-            end_node_type: 'topic',
-            graph_id: 'rto_graph',
-            kafka_grp_prop: 'producer_default',
-            property_key: 'way4-issuing-operation.output.topic.name'
-        }
-    const asd1 =  {
-        id: 'asd1',
-
-        beg_node_id: 'uasp-streaming-unp-convertor~way4-issuing-operation',
-        beg_node_type: 'flink_srv',
-        common_name: 'Не задан',
-        end_node_id: 'dev_ivr__uasp_realtime__input_converter__way4_issuing_operation__json',
-        end_node_type: 'topic',
-        graph_id: 'rto_graph',
-        kafka_grp_prop: 'producer_default',
-        property_key: 'way4-issuing-operation.output.topic.name'
+            )
     }
 
     return (
@@ -94,10 +55,13 @@ export function NewEditorTable() {
             {isLoadingTableData && <p className="text-center">Loading...</p>}
             {isErrorTableData && <p className="text-center text-red-600">Не удалось получить данные по таблице</p>}
             {!isLoadingTableData && tableData &&
-                <DataGrid
-                            // rows={[asd, asd1]}
-                            rows={getColumnsData(tableData)}
-                          columns={ getColumns(tableData)}
+                <DataGrid autoHeight
+                          checkboxSelection
+                          components={{
+                              Toolbar: GridToolbar,
+                          }}
+                          rows={getColumnsData(tableData)}
+                          columns={getColumns(tableData)}
 
 
                 />
